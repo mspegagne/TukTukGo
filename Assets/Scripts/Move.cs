8501aa2199d2427613eb249d01215ecb5fcd36e1
mspegagne <mathieu.sge@gmail.com>
2015-02-13 17:21:33 +0530

    tuktuk jump

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Simply moves the current game object
+/// </summary>
+public class Move : MonoBehaviour
+{
+	// 1 - Designer variables
+	
+	/// <summary>
+	/// Object speed
+	/// </summary>
+	public Vector2 speed = new Vector2(5, 5);
+	
+	/// <summary>
+	/// Moving direction
+	/// </summary>
+	public Vector2 direction = new Vector2(-1, 0);
+	
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 2 - Movement
+		movement = new Vector2(
+			speed.x * direction.x,
+			speed.y * direction.y);
+	}
+	
+	void FixedUpdate()
+	{
+		// Apply movement to the rigidbody
+		rigidbody2D.velocity = movement;
+	}
+}
